444e338d95103f9a50226dc96a304d1ddd9a7d2a
leejason <13131412952@163.com>
2023-05-18 10:00:34 +0800

    网页表单中输入中文,提交到后端，保存到数据库中的是unicode。要在admin中正常使用，需要encode("utf8")——解决访问admin中的某个页面出现UnicodeEncodeError: 'ascii' codec can't encode characters 错误的问题

--- simpleDjangoApp/models.py
+++ simpleDjangoApp/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class myClass(models.Model):
     caption  = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.caption.encode("utf8")
+
+
 class myStudent(models.Model):
     name = models.CharField(max_length=32)
     cls = models.ForeignKey("myClass")
